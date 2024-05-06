434a2ba60b8bf30af7d45349c27229c157b24cef
Pablo Pusiol <pablo.pusiol@gmail.com>
2024-05-06 18:49:53 -0300

    Update winget.ps1

--- winget.ps1
+++ winget.ps1
@@ -1,20 +1,44 @@
 # List of packages to install
 $packages = @(
     "Balena.Etcher",
+    "Bitwarden.Bitwarden",
+    "Datronicsoft.SpacedeskDriver.Server",
     "Debian.Debian",
+    "Docker.DockerDesktop",
+    "Eugeny.Tabby",
+    "Figma.Figma",
+    "GitHub.GitHubDesktop",
+    "Google.AndroidStudio",
+    "Google.Chrome",
+    "JetBrains.PyCharm.Professional",
+    "JetBrains.Rider",
+    "JetBrains.Toolbox",
     "Lenovo.DockManager",
     "Lenovo.SystemUpdate",
     "Logitech.GHUB",
     "Logitech.OptionsPlus",
+    "Meld.Meld",
+    "Microsoft.VisualStudioCode",
     "Microsoft.WindowsTerminal",
-    "MiniTool.PartitionWizard.Free",
+    "MullvadVPN.MullvadVPN",
+    "Notepad++.Notepad++",
+    "Prusa3D.PrusaSlicer",
     "RaspberryPiFoundation.RaspberryPiImager",
+    "Termius.Termius",
     "TortoiseSVN.TortoiseSVN",
+    "VideoLAN.VLC",
+    "VMware.WorkstationPro",
     "WhatsApp.WhatsApp",
-    "Eugeny.Tabby"
+    "ZeroTier.ZeroTierOne"
 )
+
+# Remove duplicates and sort the packages alphabetically
+$packages = $packages | Sort-Object -Unique
 
 # Loop through each package and install it
 foreach ($package in $packages) {
     winget install -e --id $package
 }
+
+# Upgrade all installed packages
+winget upgrade --all
